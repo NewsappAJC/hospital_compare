542f1a78eb08085710e221a07be44f6eb5c4f43f
John Perry <jgperry@gmail.com>
2013-04-08 17:10:16 -0400

    combined hospital comparison with detail; todo: fix links to stop default action

--- src/scripts/hospitals.js
+++ src/scripts/hospitals.js
@@ -2,15 +2,14 @@ $(function() {
 	"use strict";
 
 	d3.csv("data/hospitals.csv", function(data) {
-		window.data = data;
 		drawDotChart(data);
+		detail();
 	});
 
 	function drawDotChart(dataset) {
 		var margin = {top: 20, right: 10, bottom: 20, left: 10},
 		    w = 750 - margin.left - margin.right,
 		    h = (25 * dataset.length) - margin.top - margin.bottom,
-		    //pad = 8,
 		    left_pad = 235;
 
 		var max = Math.max(
@@ -139,7 +138,8 @@ $(function() {
 			.data(dataset)
 			.enter()
 			.append('a')
-				.attr('xlink:href', function(d){ return 'detail.html?id=' + d.provider_id })
+				// .attr('xlink:href', function(d){ return 'detail.html?id=' + d.provider_id })
+				.attr('xlink:href', '#')
 				.attr('class', 'hospital')
 			.append('text')
 				.attr('class', 'hospital')
@@ -148,6 +148,10 @@ $(function() {
 				.attr('text-anchor', 'right')
 				.attr('font-size', '13px')
 				.text(function(d){ return d.hospital_name;})
+			.on('click', function(d) {
+					updateDetail(d.provider_id);
+					return false;
+			})
 			.on('mouseover', function(){ 
 				return d3.select(this)
 									.attr('fill','grey')
@@ -172,8 +176,6 @@ $(function() {
 		// sort by different infection sources
 		d3.select('#sortby').on('change', function() {
 				var infection = this.value;
-
-				var transition = svg.transition().duration(1000).ease('bounce');
 
 				svg.selectAll('.clabsi')
 					.sort(function(a,b){
@@ -231,5 +233,215 @@ $(function() {
 				tt.select('#score').text('score: ' + d.clabsi_ratio);
 				d3.select('#tooltip').classed('hidden', false);
 			}
+	  }
+
+	  function detail(id) {
+			var config = {
+				w: 750,
+		    h: 120,
+		    leftMargin: 10,
+		    rightMargin: 50,
+		    topMargin: 45,
+		    red: '#FF0000',
+		    green: '#088A08',
+		    lightgreen: '#CEF6CE',
+		    grey: '#A4A4A4',
+		    id: id || '110079'
+			};
+			window.config = config;
+
+			var legendSvg = d3.select('#legend')
+				.append('svg')
+					.attr('width', config.w)
+					.attr('height', 60);
+
+			var legend = legendSvg.append('g');
+
+			legend.append('text').text('Expected cases:')
+					.attr('x', 5)
+					.attr('y', 25);
+			legend.append('line')
+				.attr('x1', 108)
+				.attr('y1', 10)
+				.attr('x2', 108)
+				.attr('y2', 30)
+				.attr('stroke', config.grey)
+				.attr('stroke-width', 5);
+			legend.append('text').text('Expected range:')
+					.attr('x', 125)
+					.attr('y', 25);
+			legend.append('rect')
+				.attr('x', 225)
+				.attr('y', 10)
+				.attr('width', 20)
+				.attr('height', 20)
+				.attr('fill', config.lightgreen);
+			legend.append('text').text('Actual cases, fewer than expected:')
+					.attr('x', 260)
+					.attr('y', 25);
+			legend.append('line')
+				.attr('x1', 470)
+				.attr('y1', 10)
+				.attr('x2', 470)
+				.attr('y2', 30)
+				.attr('stroke', config.green)
+				.attr('stroke-width', 5);
+			legend.append('text').text('more than expected:')
+					.attr('x', 485)
+					.attr('y', 25);
+			legend.append('line')
+				.attr('x1', 608)
+				.attr('y1', 10)
+				.attr('x2', 608)
+				.attr('y2', 30)
+				.attr('stroke', config.red)
+				.attr('stroke-width', 5);
+
+			d3.csv("data/detail.csv", function(data) {
+				window.data = data;
+				drawDetailChart(data, 'CLABSI',   config);
+				drawDetailChart(data, 'CAUTI',    config);
+				drawDetailChart(data, 'SSIcolon', config);
+			});
+
+			function drawDetailChart(data, source, config) {
+				var provider  = _.findWhere(data, {provider_id: config.id});
+				window.provider = provider;
+				var observed  = provider[source + '_observed'],
+				    predicted = provider[source + '_predicted'],
+				    ratio     = provider[source + '_ratio'],
+				    upper     = provider[source + '_lower'] > 0 ? observed / provider[source + '_lower'] : 0,
+				    lower     = provider[source + '_upper'] > 0 ? observed / provider[source + '_upper'] : 0;
+
+				var max = d3.max( data, function(d){return parseInt(d[source + '_observed']);}),
+				    scale = d3.scale.linear()
+					  	.domain([0,max + 1])
+					  	.range([config.leftMargin, config.w - config.rightMargin]);
+
+				d3.select('#hospital_name').text(provider.hospital_name);
+
+				var svg = d3.select('#' + source.toLowerCase() + '-detail' )
+					.append('svg')
+						.attr('width', config.w)
+						.attr('height', config.h)
+						.attr('class', 'svg-detail');
+
+				// Headline
+				svg.append('text')
+					.text('(ratio: ' + (ratio > 0 ? ratio : 'NA') + ')')
+					.attr('id', 'ratio-' + source)
+					.attr('x', 5)
+					.attr('y', 15);
+
+				var chart = svg.append('g');
+
+				if (lower > 0 & upper > 0 ) {
+					chart.append('rect')
+						.attr('id', 'range-' + source)
+						.attr('x', function(){ return scale(lower); })
+						.attr('y', 3 + config.topMargin )
+						.attr('width', function(){ return scale(upper - lower); })
+						.attr('height', 17)
+						.attr('fill', config.lightgreen);
+				}
+
+				// predicted value marker
+				chart.append('line')
+					.attr('id', 'predicted-' + source)
+					.attr('x1', function(){ return scale(predicted); })
+					.attr('y1', 3 + config.topMargin)
+					.attr('x2', function(){ return scale(predicted); })
+					.attr('y2', 20 + config.topMargin)
+					.attr('stroke', config.grey)
+					.attr('stroke-width', Math.abs(observed - predicted) >= (max/20) ? 5 : 7);
+				chart.append('text').text( predicted )
+					.attr('id', 'predicted-text-' + source)
+					.attr('x', function(){ return scale(predicted); } )
+					.attr('y', Math.abs(observed - predicted) >= (max/20) ? config.topMargin : config.topMargin - 13)
+					.attr('text-anchor', 'middle');
+
+				// observed value marker
+				chart.append('line')
+					.attr('id', 'observed-' + source)
+					.attr('x1', function(){ return scale(observed); })
+					.attr('y1', 3 + config.topMargin)
+					.attr('x2', function(){ return scale(observed); })
+					.attr('y2', 20 + config.topMargin)
+					.attr('stroke', function(){ return parseFloat(observed) > parseFloat(upper) ? config.red : config.green; })
+					.attr('stroke-width', 5);
+				chart.append('text').text( observed )
+					.attr('id', 'observed-text-' + source)
+					.attr('x', function(){ return scale(observed); } )
+					.attr('y', config.topMargin)
+					.attr('text-anchor', 'middle');
+
+				// Axis
+				var axis = d3.svg.axis()
+					.scale(scale)
+					.orient('bottom')
+					.ticks(7);
+
+				svg.append('g')
+					.attr('class', 'x-axis')
+					.attr('transform', 'translate(0,' + (20 + config.topMargin) + ')' )
+					.call(axis);
+			}
+		}
+	  var updateDetail = function(id) {
+			d3.csv("data/detail.csv", function(data) {
+	  		var provider  = _.findWhere(data, {provider_id: id});
+	  		d3.select('#hospital_name')
+	  			.transition().delay(700)
+	  			.text(provider.hospital_name);
+
+	  		['CLABSI','CAUTI','SSIcolon'].forEach( function(source) {
+	  			console.log(source);
+			    var observed  = provider[source + '_observed'],
+					    predicted = provider[source + '_predicted'],
+					    ratio     = provider[source + '_ratio'],
+					    upper     = provider[source + '_lower'] > 0 ? observed / provider[source + '_lower'] : 0,
+					    lower     = provider[source + '_upper'] > 0 ? observed / provider[source + '_upper'] : 0;
+
+					var max = d3.max( data, function(d){return parseInt(d[source + '_observed']);}),
+					    scale = d3.scale.linear()
+						  	.domain([0,max + 1])
+						  	.range([config.leftMargin, config.w - config.rightMargin]);
+
+	  			d3.select('#ratio-' + source).text('(ratio: ' + (ratio > 0 ? ratio : 'NA') + ')');
+
+	  			d3.select('#range-' + source)
+	  				.transition().duration(1000)
+						.attr('x', function(){ return scale(lower); })
+						.attr('y', 3 + config.topMargin )
+						.attr('width', function(){ return scale(upper - lower); });
+
+					d3.select('#predicted-' + source)
+	  				.transition().duration(1000)
+							.attr('x1', function(){ return scale(predicted); })
+							.attr('y1', 3 + config.topMargin)
+							.attr('x2', function(){ return scale(predicted); })
+							.attr('y2', 20 + config.topMargin)
+					d3.select('#predicted-text-' + source)
+						.transition().duration(1000)
+						.text( predicted )
+							.attr('x', function(){ return scale(predicted); } )
+							.attr('y', Math.abs(observed - predicted) >= (max/20) ? 45 : 32)
+							.attr('text-anchor', 'middle');
+
+					d3.select('#observed-' + source)
+	  				.transition().duration(1000)
+							.attr('x1', function(){ return scale(observed); })
+							.attr('y1', 3 + config.topMargin)
+							.attr('x2', function(){ return scale(observed); })
+							.attr('y2', 20 + config.topMargin)
+							.attr('stroke', function(){ return parseFloat(observed) > parseFloat(upper) ? config.red : config.green; })
+					d3.select('#observed-text-' + source)
+						.transition().duration(1000)
+						.text( observed )
+							.attr('x', function(){ return scale(observed); } )
+							.attr('y', config.topMargin)
+							.attr('text-anchor', 'middle');
+		  	});
+	  	});
 	  }
 });
